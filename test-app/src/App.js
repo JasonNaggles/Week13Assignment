9c04ed8fe0725ea1ef74870d5863a6ac1b21d65a
JasonNaggles <JasonNaggles.verizon.net>
2023-09-04 23:03:05 -0400

    first commit

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Navigation />
     </div>
+
   );
 }
 
